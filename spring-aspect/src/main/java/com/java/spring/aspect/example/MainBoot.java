39ee2fe37efbb968f635197f01f0b0279ff1a42c
lixinyou <lxytsp@163.com>
2017-08-16 20:37:39 +0800

    add generate proxy class file

--- spring-aspect/src/main/java/com/java/spring/aspect/example/MainBoot.java
+++ spring-aspect/src/main/java/com/java/spring/aspect/example/MainBoot.java
@@ -1,12 +1,13 @@
 package com.java.spring.aspect.example;
 
 import com.java.spring.aspect.example.component.ExampleAction;
+import java.io.IOException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainBoot {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     String app = "/application.xml";
     ApplicationContext context = new ClassPathXmlApplicationContext(app);
     ExampleAction action = context.getBean("exampleAction", ExampleAction.class);
--- spring-aspect/src/main/java/com/java/spring/aspect/example/component/ExampleAction.java
+++ spring-aspect/src/main/java/com/java/spring/aspect/example/component/ExampleAction.java
@@ -1,8 +1,11 @@
 package com.java.spring.aspect.example.component;
 
 import com.java.spring.aspect.example.service.ExampleService;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import sun.misc.ProxyGenerator;
 
 @Component(value = "exampleAction")
 public class ExampleAction {
@@ -10,7 +13,27 @@ public class ExampleAction {
   @Autowired
   private ExampleService exampleService;
 
-  public void welcomeAspect() {
+  public void welcomeAspect() throws IOException {
+    Class<? extends ExampleService> cls = exampleService.getClass();
+    createProxyClassFile();
     exampleService.sayHello();
+  }
+
+
+  private static void createProxyClassFile() throws IOException {
+    String name = "exampleService";
+    byte[] data = ProxyGenerator.generateProxyClass(name, new Class[]{ExampleService.class});
+    FileOutputStream out = null;
+    try {
+      out = new FileOutputStream(name + ".class");
+      out.write(data);
+      out.close();
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      if (out != null) {
+        out.close();
+      }
+    }
   }
 }
